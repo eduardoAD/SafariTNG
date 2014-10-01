dafa07f3dd994f6709cca248f1898205897521fe
Eduardo Alvarado Díaz <GLB-MXM0001@Eduardos-MacBook-Pro-2.local>
2014-10-01 10:19:56 -0500

    Initial Commit

---
+++ SafariTNG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SafariTNG
+//
+//  Created by Eduardo Alvarado Díaz on 10/1/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
