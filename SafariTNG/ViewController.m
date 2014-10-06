8b0170001bc79627a01daacfd0d483cd6ba4ec6e
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-06 01:34:03 -0500

    initial setup

--- SafariTNG/ViewController.m
+++ SafariTNG/ViewController.m
@@ -8,20 +8,54 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () < UIWebViewDelegate, UITextFieldDelegate, UIAlertViewDelegate>
+@property (weak,nonatomic) IBOutlet UIWebView *webView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self loadHomePage];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    UIAlertView *alertView = [[UIAlertView alloc] init];
+    alertView.delegate = self;
+    alertView.title = @"Bad error... No Biscuit";
+    alertView.message = error.localizedDescription;
+    [alertView addButtonWithTitle:@"That's all"];
+    [alertView addButtonWithTitle:@"Go home"];
+    [alertView show];
+}
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex{
+    if (buttonIndex == 1){
+        [self loadHomePage];
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self loadURLString:textField.text];
+    return YES;
+}
+
+- (void) loadURLString: (NSString *)urlString {
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:urlRequest];
+}
+
+- (void) loadHomePage {
+    [self loadURLString:@"http://www.mobilemakers.co"];
 }
 
 @end
